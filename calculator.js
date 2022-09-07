52cd71d422f79c651138b82cdcfbb474e0d2a855
harshal239 <harshalwalunj49@gmail.com>
2022-09-08 01:47:26 +0530

    Add : Returns answer for addition of numbers in string

--- calculator.js
+++ calculator.js
@@ -3,6 +3,12 @@ const add = (numbers) => {
   if (numbers.length === 0) {
     return answer;
   }
+
+  const splittedString = numbers.split(",");
+
+  for (let index = 0; index < splittedString.length; index++) {
+    answer += parseInt(splittedString[index]);
+  }
   return answer;
 };
 module.exports = add;
--- calculator.spec.js
+++ calculator.spec.js
@@ -3,3 +3,7 @@ const add = require("./calculator");
 test("Returns 0 for empty String", () => {
   expect(add("")).toBe(0);
 });
+
+test("Returns Answer after all numbers in input string", () => {
+  expect(add("1,2,3")).toBe(6);
+});
